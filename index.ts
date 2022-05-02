e31a1ced857a55b74d2f84309df3a25c94a6130f
minamorl <minamorl@users.noreply.github.com>
2022-05-03 05:06:03 +0900

    Initial commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+const generateProxy: any = (parentTarget: any, propertyKey: string, [ref, ...rest]: string[], searched: string[], id: string) => {
+  return new Proxy(parentTarget, {
+    get: (target, p) => {
+      const searched_tmp = searched.concat([ref, ...rest])
+      if (searched_tmp.join("") === id){
+        return target[propertyKey]
+      }
+      return generateProxy(parentTarget, propertyKey, rest, searched.concat([ref]), id)
+    }
+  })
+}
+
+const path: (...args: string[]) => MethodDecorator = function(...args) {
+  return (target, propertyKey, descriptor) => {
+    const p = generateProxy(target, propertyKey, [...args], [], args.join(""));
+    const resolver_proxies: Map<string, any> = (target as any)[Symbol.for('resolver_proxies')] ?? new Map();
+    resolver_proxies.set(args.join(""), p);
+    (target as any)[Symbol.for('resolver_proxies')] = resolver_proxies;
+    return descriptor 
+  }
+}
+
+class Example {
+  resolvers: (searcheed: string[]) => any = (searched = []) => new Proxy(this, {
+      get: (target, handler) => {
+        let proxies = this.proxies();
+        
+        if (proxies.has([...searched, handler.toString()].join(""))) {
+          return proxies.get([...searched, handler.toString()].join(""))[handler]
+        }
+        return this.resolvers([...searched, handler.toString()])
+      }
+    })
+  private proxies(): Map<string, any> {
+    return ((this as any)[Symbol.for('resolver_proxies')] as Map<string, any>)
+  }
+  @path("div", "div")
+  test() {
+    return "test1"
+  }
+  @path("h1", "div", "span")
+  test2() {
+    return 'test2'
+  }
+}
+
+const instance = new Example() as any
+console.log('Example', instance.resolvers().div.div())
+// console.log('Example', instance.resolvers().div.div.div.div)
+console.log('Example', instance.resolvers().h1.div.span())
